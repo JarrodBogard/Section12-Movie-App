4b80dcbd7441979e85a17bb4899acd1f561cd6a5
Jarrod Bogard <jarrodbogard@gmail.com>
2024-03-07 17:47:07 -0500

    update project

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,141 +1,88 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const API_KEY = "ff0a7ec6";
-
 // structural component //
 export default function App() {
-  const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
-  const [query, setQuery] = useState("inception");
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [selectedId, setSelectedId] = useState(null);
-  // const tempQuery = "interstellar";
-
-  /*
-  useEffect(function () {
-    console.log("After initial render"); // runs second because effects are not executed until after the browser paint
-  }, []);
-
-  useEffect(function () {
-    console.log("After every render"); // runs third simply because it follows the above useEffect
-  });
-
-  useEffect(
-    function () {
-      console.log("After dependency is updated");
-    },
-    [query]
-  );
-
-  console.log("During render"); // runs first as part of the render logic, during render
-  */
-
-  function handleSelectMovie(id) {
-    // setSelectedId(id);
-    setSelectedId((currentId) => (id === currentId ? null : id));
-  }
-
-  function handleCloseMovie() {
-    setSelectedId(null);
-  }
-
-  useEffect(
-    function () {
-      // fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=interstellar`)
-      //   .then((res) => res.json())
-      //   .then((data) => setMovies(data.Search));
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError(""); // ensures error object is reset before each fetch request to prevent unintentional errors
-          const response = await fetch(
-            `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
-          );
-
-          if (!response.ok) {
-            throw new Error("Something went wrong. Unable to fetch movies.");
-          }
-
-          const data = await response.json();
-
-          if (data.Response === "False") {
-            throw new Error("Movie not found.");
-          }
-
-          setMovies(data.Search);
-          // setIsLoading(false);
-          // console.log(data.Search); // logs movie data
-          // console.log(movies); // won't work because state is asynchronous and therefore this will be stale state
-        } catch (error) {
-          // console.error(error);
-          console.error(error.message);
-          setError(error.message);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-
-      // if (!query.length) {
-      if (query.length < 3) {
-        setMovies([]);
-        setError("");
-        return; // the fetch request will not be executed if the condition is not met
-      }
-
-      fetchMovies();
-    },
-    [query]
-  );
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
       <NavBar>
-        <Search query={query} setQuery={setQuery} />
+        <Search />
         <NumResults movies={movies} />
       </NavBar>
       <Main>
         {/* implicit approach to passing components as children */}
         <Box>
-          {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
-          {isLoading && <Loader />}
-          {!isLoading && !error && (
-            <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
-          )}
-          {error && <ErrorMessage message={error} />}
+          <MovieList movies={movies} />
         </Box>
         <Box>
-          {selectedId ? (
-            <MovieDetails
-              selectedId={selectedId}
-              onCloseMovie={handleCloseMovie}
-            />
-          ) : (
-            <>
-              <WatchedSummary watched={watched} />
-              <WatchedMovieList watched={watched} />
-            </>
-          )}
+          <WatchedSummary watched={watched} />
+          <WatchedMovieList watched={watched} />
         </Box>
       </Main>
+
+      {/* explicit approach to passing components as props */}
+      {/* <Box element={<MovieList movies={movies} />} />
+      <Box
+        element={
+          <>
+            <WatchedSummary watched={watched} />
+            <WatchedMovieList watched={watched} />
+          </>
+        }
+      /> */}
     </>
-  );
-}
-
-// presentational component //
-function Loader() {
-  return <p className="loader">Loading...</p>;
-}
-
-// presentational component //
-function ErrorMessage({ message }) {
-  return (
-    <p className="error">
-      <span>⛔</span> {message}
-    </p>
   );
 }
 
@@ -160,7 +107,9 @@ function Logo() {
 }
 
 // stateful component //
-function Search({ query, setQuery }) {
+function Search() {
+  const [query, setQuery] = useState("");
+
   return (
     <input
       className="search"
@@ -200,21 +149,21 @@ function Box({ children }) {
 }
 
 // stateful component //
-function MovieList({ movies, onSelectMovie }) {
+function MovieList({ movies }) {
   // const [movies, setMovies] = useState(tempMovieData);
   return (
-    <ul className="list list-movies">
+    <ul className="list">
       {movies?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
+        <Movie movie={movie} key={movie.imdbID} />
       ))}
     </ul>
   );
 }
 
 // presentational component //
-function Movie({ movie, onSelectMovie }) {
+function Movie({ movie }) {
   return (
-    <li onClick={() => onSelectMovie(movie.imdbID)}>
+    <li>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
@@ -224,17 +173,6 @@ function Movie({ movie, onSelectMovie }) {
         </p>
       </div>
     </li>
-  );
-}
-
-function MovieDetails({ selectedId, onCloseMovie }) {
-  return (
-    <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
-      {selectedId}
-    </div>
   );
 }
 
